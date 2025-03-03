893d6498e8382150af17dfd7373eb3bc10edd436
Gustave Legrand <legrandgustave2001@gmail.com>
2025-03-03 18:57:06 +0100

    Add Bootstrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import 'bootstrap/dist/css/bootstrap.css';
+    import scriptSrc from 'bootstrap/dist/js/bootstrap.bundle.js?url';
+</script>
+<svelte:head>
+    <script src={scriptSrc}></script>
+</svelte:head>
+<div class="container py-4 px-3 mx-auto">
+    <h1>Hello, Bootstrap and Vite!</h1>
+    <button class="btn btn-primary">Primary button</button>
+</div>
