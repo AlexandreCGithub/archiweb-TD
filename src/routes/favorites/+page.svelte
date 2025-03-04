343a7489d58b7818b2701d9502fe8de148194c4c
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2025-03-04 15:44:21 +0100

    modifying titles

--- src/routes/favorites/+page.svelte
+++ src/routes/favorites/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 </script>
 
+<svelte:head>
+    <title>Favorites</title>
+</svelte:head>
+
 <div class="row text-start mb-4">
     <div class="col-md-8 d-flex flex-column justify-content-center">
         <div class="d-flex justify-content-between align-items-center">
