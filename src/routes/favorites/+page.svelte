7e5ecc77df16607b13b8b0123ee5e2f452dd941d
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2025-03-04 15:35:23 +0100

    hello world page

---
+++ src/routes/favorites/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<div class="row text-start mb-4">
+    <div class="col-md-8 d-flex flex-column justify-content-center">
+        <div class="d-flex justify-content-between align-items-center">
+            <span class="display-4">Hello, World!</span>
+            <span class="display-6 text-body-emphasis">Welcome</span>
+        </div>
+
+    </div>
+</div>
