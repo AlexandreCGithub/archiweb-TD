d056e33ce64ff97d6fdc2d10595108966b1c673b
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2025-03-09 01:15:43 +0100

    token is get; for now printed

--- src/lib/components/NavBar.ts
+++ src/lib/components/NavBar.ts
@@ -1,18 +1,23 @@
 export const submitLogin = async (username: string, password: string): Promise<boolean> => {
     try {
-      const response = await fetch('/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username,
-          password
-        })
-      });
-  
+        const response = await fetch('https://gourmet.cours.quimerch.com/login', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json, application/xml',
+              'Content-Type': '*/*',
+            },
+            body: JSON.stringify({
+              username: username,
+              password: password
+            })
+          });
       if (response.ok) {
         console.log("Connexion réussie");
+        const responseData = await response.json();
+        console.log(responseData)
+
+
+
         return true;
       } else {
         console.log("Erreur de connexion");
@@ -20,6 +25,7 @@ export const submitLogin = async (username: string, password: string): Promise<b
       }
     } catch (error) {
       console.error("Erreur lors de la connexion :", error);
+
       return false;
     }
   };
