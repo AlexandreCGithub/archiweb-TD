f38b3507b31704aab7c4b51de86dd7cede493000
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2025-03-09 21:34:02 +0100

    fetching favorites

--- src/routes/favorites/+page.server.ts
+++ src/routes/favorites/+page.server.ts
@@ -1,1 +1,14 @@
 import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const response = await fetch(`https://gourmet.cours.quimerch.com/users/chi/favorites`, {
+		headers: {
+			Accept: 'application/json, application/xml'
+		}
+	});
+	const data = await response.json();
+	return {
+		recipe: data
+	};
+};
+
