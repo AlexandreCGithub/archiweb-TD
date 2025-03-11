41cd1ca0895552249336f6570855530b9640889d
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2025-03-11 11:23:46 +0100

    fixing error page

--- src/routes/favorites/+page.server.ts
+++ src/routes/favorites/+page.server.ts
@@ -1,10 +1,11 @@
 import type { PageServerLoad } from './$types';
 import type { Recipe } from '$lib/types/Recipe';
 
-export const load: PageServerLoad = async () => {
-	const response = await fetch(`https://gourmet.cours.quimerch.com/users/chi/favorites`, {
+export const load: PageServerLoad = async ({cookies}) => {
+	const response = await fetch(`https://gourmet.cours.quimerch.com/favorites`, {
 		headers: {
-			Accept: 'application/json, application/xml'
+			Accept: 'application/json, application/xml',
+			Authorization: `Bearer ${cookies.get("token")}`
 		}
 	});
 	const data = await response.json();
