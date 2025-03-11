77125e05d65e5854643fabae7785660d50eb9966
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2025-03-11 16:05:32 +0100

    corriger tests

--- src/lib/components/NavBar.svelte.test.ts
+++ src/lib/components/NavBar.svelte.test.ts
@@ -3,19 +3,19 @@ import { render, screen } from '@testing-library/svelte';
 import NavBar from './NavBar.svelte';
 
 describe('NavBar Component', () => {
-	test('affiche le lien Home', () => {
+	test('affiche le lien Accueil', () => {
 		render(NavBar, { data: { token: null, data: undefined, status: undefined } });
 
-		// Vérifie que le lien "Home" est bien affiché
-		const homeLink = screen.getByText('Home');
+		// Vérifie que le lien "Accueil" est bien affiché
+		const homeLink = screen.getByText('Accueil');
 		expect(homeLink).toBeInTheDocument();
 	});
 
-	test('affiche le bouton Login', () => {
+	test('affiche le bouton Se connecter', () => {
 		render(NavBar);
 
-		// Vérifie que le bouton "Login" est bien affiché
-		const loginButton = screen.getByText('Login');
+		// Vérifie que le bouton "Se connecter" est bien affiché
+		const loginButton = screen.getByText('Se connecter');
 		expect(loginButton).toBeInTheDocument();
 	});
 });
