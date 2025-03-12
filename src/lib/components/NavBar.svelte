14c969bad24b472d7c37d31d729f24b9bb48704e
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2025-03-12 19:50:07 +0100

    hide favorites if not connected

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -63,7 +63,9 @@
 	<div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
 		<ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
 			<li><a href="/" class="nav-link px-2 text-white">Accueil</a></li>
-			<li><a href="/favorites" class="nav-link px-2 text-white">Favoris ⭐</a></li>
+			{#if userPseudo}
+				<li><a href="/favorites" class="nav-link px-2 text-white">Favoris ⭐</a></li>
+			{/if}
 		</ul>
 
 		<form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
