63930d1f6374ec2c18d0d79c739a7fc90cb89893
Magnoir <legrandgustave2001@gmail.com>
2025-03-16 23:23:54 +0100

    Factozire Main Page Component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,12 @@
 <script lang="ts">
 	import type { Recipe } from '$lib/types/Recipe';
-	import { searchValue } from '$lib/stores/search';
-	import TitleBanner from '$lib/components/TitleBanner.svelte';
-	import RecipeCard from '$lib/components/RecipeCard.svelte';
+	import MainPage from '$lib/components/MainPage.svelte';
 
 	let { data } = $props();
-	let recipes: Recipe[] = data.recipes;
-
-	function removeAccents(str: string): string {
-		return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-	}
-
-	let filteredRecipes: Recipe[] = $state([]);
-
-	$effect(() => {
-		filteredRecipes = recipes.filter((recipe) =>
-			removeAccents(recipe.name.toLowerCase()).includes(removeAccents($searchValue.toLowerCase()))
-		);
-	});
-	const titleData = { title: 'Marmitron', subtitle: 'Découvrez nos recettes' };
+	let dataRecipes: Recipe[] = data.recipes;
+	let title = 'Marmitron';
+	let subtitle = 'Découvrez nos recettes';
+	let isPageFavorite = false;
 </script>
 
-<TitleBanner {titleData} />
-
-<div class="row">
-	{#each filteredRecipes as recipe (recipe.id)}
-		{#if recipe.published}
-			<RecipeCard {recipe} />
-		{/if}
-	{/each}
-</div>
+<MainPage {title} {subtitle} {dataRecipes} {isPageFavorite} />
