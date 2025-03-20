a6da7a4096678ee1553acde488dd1fa37dc4c07f
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2025-03-20 19:14:28 +0100

    add tests

--- src/lib/components/RecipeCard.svelte.test.ts
+++ src/lib/components/RecipeCard.svelte.test.ts
@@ -13,11 +13,34 @@ describe('RecipeCard Component', () => {
 		cook_time: 45
 	};
 
-	test('affiche correctement les informations de la recette', () => {
+	test('affiche correctement le nom de la recette', () => {
 		render(RecipeCard, { props: { recipe } });
 
 		expect(screen.getByText(recipe.name)).toBeInTheDocument();
+	});
+
+	test('affiche correctement l\'image de la recette', () => {
+		render(RecipeCard, { props: { recipe } });
 
 		expect(screen.getByAltText(recipe.name)).toHaveAttribute('src', recipe.image_url);
+	});
+
+	test('affiche correctement la description de la recette', () => {
+		render(RecipeCard, { props: { recipe } });
+
+		const description = screen.getByText(recipe.description.slice(0, 30) + '...');
+		expect(description).toBeInTheDocument();
+	});
+
+	test('affiche correctement le badge when_to_eat', () => {
+		render(RecipeCard, { props: { recipe } });
+
+		expect(screen.getByText(recipe.when_to_eat)).toBeInTheDocument();
+	});
+
+	test('affiche le bouton Voir la recette', () => {
+		render(RecipeCard, { props: { recipe } });
+
+		expect(screen.getByText('Voir la recette')).toBeInTheDocument();
 	});
 });
