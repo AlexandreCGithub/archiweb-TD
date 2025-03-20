323757ead1f5f3f8e762728f71e274b5a68273fb
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2025-03-20 02:38:17 +0100

    format & lint

--- src/lib/api/favoritesApi.ts
+++ src/lib/api/favoritesApi.ts
@@ -1,33 +1,39 @@
 export const fetchMyFavorites = async (token: string) => {
-    const response = await fetch(`https://gourmet.cours.quimerch.com/favorites`, {
-        headers: {
-            Accept: 'application/json, application/xml',
-            Authorization: `Bearer ${token}`
-        }
-    });
-    return response;
+	const response = await fetch(`https://gourmet.cours.quimerch.com/favorites`, {
+		headers: {
+			Accept: 'application/json, application/xml',
+			Authorization: `Bearer ${token}`
+		}
+	});
+	return response;
 };
 
-export const addAFavorite = async (userPseudo:string, recipeID:string,token: string) => {
-    const response = await fetch(`https://gourmet.cours.quimerch.com/users/${userPseudo}/favorites?recipeID=${recipeID}`, {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json, application/xml',
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`
-        }
-    });
-    return response;
+export const addAFavorite = async (userPseudo: string, recipeID: string, token: string) => {
+	const response = await fetch(
+		`https://gourmet.cours.quimerch.com/users/${userPseudo}/favorites?recipeID=${recipeID}`,
+		{
+			method: 'POST',
+			headers: {
+				Accept: 'application/json, application/xml',
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`
+			}
+		}
+	);
+	return response;
 };
 
-export const deleteAFavorite = async (userPseudo:string, recipeID:string,token: string) => {
-    const response = await fetch(`https://gourmet.cours.quimerch.com/users/${userPseudo}/favorites?recipeID=${recipeID}`, {
-        method: 'DELETE',
-        headers: {
-            Accept: 'application/json, application/xml',
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`
-        }
-    });
-    return response;
+export const deleteAFavorite = async (userPseudo: string, recipeID: string, token: string) => {
+	const response = await fetch(
+		`https://gourmet.cours.quimerch.com/users/${userPseudo}/favorites?recipeID=${recipeID}`,
+		{
+			method: 'DELETE',
+			headers: {
+				Accept: 'application/json, application/xml',
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`
+			}
+		}
+	);
+	return response;
 };
