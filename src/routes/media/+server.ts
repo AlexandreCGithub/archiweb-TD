e5dcf2c61e4374d6d163efe57c594933c9ef9e31
Magnoir <legrandgustave2001@gmail.com>
2025-03-25 13:13:09 +0100

    Handle erros in proxy

--- src/routes/media/+server.ts
+++ src/routes/media/+server.ts
@@ -3,6 +3,15 @@ export async function GET({ url }) {
 	const response = await fetch(imageUrl);
 	const body = await response.arrayBuffer();
 
+	if (!response.ok) {
+		return new Response(imageUrl, {
+			status: 200,
+			headers: {
+				'Cache-Control': 'max-age=86400'
+			}
+		});
+	}
+
 	return new Response(body, {
 		headers: {
 			'Content-Type': response.headers.get('Content-Type') || 'application/octet-stream',
