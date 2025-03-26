16f7eedfde3a86c94b401aea8ed7656260eaf8ba
Magnoir <legrandgustave2001@gmail.com>
2025-03-26 13:17:24 +0100

    Fix tests

--- src/lib/components/FootBar.svelte.test.ts
+++ src/lib/components/FootBar.svelte.test.ts
@@ -1,11 +1,20 @@
-import { describe, test, expect } from 'vitest';
+import { describe, test, expect, vi } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import { render, screen } from '@testing-library/svelte';
 import FootBar from './FootBar.svelte';
+
+vi.mock('$env/dynamic/public', () => ({
+	env: {
+		PUBLIC_VITE_COMMIT_NUMBER: '1.0.0',
+		PUBLIC_COMMIT_NUMBER: '123456'
+	}
+}));
 
 describe('FootBar Component', () => {
 	test('Affiche le bon texte sur la FootBar', () => {
 		render(FootBar);
 		expect(screen.getByText("Site de l'équipe Chi.")).toBeInTheDocument();
+		expect(screen.getByText('Version: 1.0.0')).toBeInTheDocument();
+		expect(screen.getByText('Version: 123456')).toBeInTheDocument();
 	});
 });
