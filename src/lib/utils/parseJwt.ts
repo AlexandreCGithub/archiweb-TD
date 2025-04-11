c24431325255061450d1dd297333e127612f4e50
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2025-04-11 21:12:20 +0200

    adding test for parseJwt

--- src/lib/utils/parseJwt.ts
+++ src/lib/utils/parseJwt.ts
@@ -1,6 +1,5 @@
 export const parseJwt = (token: string | null | undefined) => {
 	if (!token) return null;
-	if (token == undefined) return undefined;
 	try {
 		return JSON.parse(atob(token.split('.')[1]));
 	} catch (e) {
